3edabee3f70fdb1129f15b48b7101cef2e681ec4
Fortis Irdmousa <moosorkh@gmail.com>
2024-11-16 14:46:58 -0800

    working on the render

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,16 +9,8 @@ import { sequelize } from './models/index.js';
 dotenv.config();
 
 const app = express();
-// const PORT = parseInt(process.env.PORT || '3001', 10);
 
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// }
 const PORT = process.env.PORT || 3001; // Convert to number, default to 3001
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
