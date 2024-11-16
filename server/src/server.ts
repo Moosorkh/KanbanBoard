cad700d9193936138be5db582c26ebc559852704
Fortis Irdmousa <moosorkh@gmail.com>
2024-11-16 00:00:28 -0800

    Listen to PORT 3001 added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,23 @@
 const forceDatabaseRefresh = false;
 
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = parseInt(process.env.PORT || '3001', 10);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// }
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001; // Convert to number, default to 3001
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
